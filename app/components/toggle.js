de239aabaa332873468a7b33ba1e3492099aed0a
mila <mila.mirovic98@gmail.com>
2024-02-07 08:57:26 +0100

    Create toggle.js

---
+++ app/components/toggle.js
@@ -0,0 +1,37 @@
+"use client";
+import { useState } from "react";
+import classNames from "classnames";
+
+export default function Toggle({ onToggle }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const toggleClasses = classNames(
+    "flex w-20 h-10 bg-gray-600 m-10 rounded-full transition-all duration-500",
+    {
+      "bg-green-500": isSelected,
+    }
+  );
+
+  const circleClasses = classNames(
+    "h-10 w-10 bg-white rounded-full transition-all duration-500",
+    {
+      "ml-10": isSelected,
+    }
+  );
+
+  function toggleClickHandler() {
+    onToggle(!isSelected);
+    setIsSelected(!isSelected);
+  }
+
+  return (
+    <div
+      onClick={toggleClickHandler}
+      className={toggleClasses}
+    >
+      <span
+        className={circleClasses}
+      />
+    </div>
+  );
+}
